f68614be5d1169067322d13f9737522b4363d402
Luca-1709 <lucascale1709@gmail.com>
2025-04-15 13:19:13 -0400

    Primera clase python

--- holaaa.py
+++ holaaa.py
@@ -11,11 +11,35 @@
 # else:
 #     print("El alumno reprobó")
 
-print("Ingrese su edad")
+# print("Ingrese su edad")
 
-edad=int(input())
+# edad=int(input())
 
-if edad>=18:
-    print("Usted es mayor de edad")
+# if edad>=18:
+#     print("Usted es mayor de edad")
+# else:
+#     print("Usted es menor de edad")
+
+
+
+# print("Ingrese su edad")
+
+# edad=int(input())
+
+# if edad < 12:
+#     print("Usted es un niño")
+# elif edad >= 12 and edad <= 17:
+#     print("Usted es adolescente")
+# else:  
+#     print("Usted es mayor de edad")
+
+n1=float(input("Ingrese un número "))
+n2=float(input("Ingrese un número "))
+n3=float(input("Ingrese un número "))
+
+if n1>n2 and n1>n3:
+    print("El numero", n1 ,"es el mayor")
+elif n2>n3:
+    print("El número", n2 ,"es el mayor")
 else:
-    print("Usted es menor de edad")
+    print("El número", n3 ,"es el mayor")
