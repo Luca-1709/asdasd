94053894224ac5b0b4105c424aed8beefe12332c
Luca-1709 <lucascale1709@gmail.com>
2025-04-15 12:45:16 -0400

    primer commit

---
+++ holaaa.py
@@ -0,0 +1,21 @@
+# print("Ingrese sus 3 notas")
+
+# n1=float(input("Ingrese su nota "))
+# n2=float(input("Ingrese su nota "))
+# n3=float(input("Ingrese su nota "))
+# promedio=float((n1+n2+n3)/3)
+# print("Su promedio es", promedio)
+
+# if promedio>=4:
+#     print("El alumno aprobó")
+# else:
+#     print("El alumno reprobó")
+
+print("Ingrese su edad")
+
+edad=int(input())
+
+if edad>=18:
+    print("Usted es mayor de edad")
+else:
+    print("Usted es menor de edad")
