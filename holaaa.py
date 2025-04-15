5718d7eca2dd292f5128546ec8b950ec71649cb5
Luca-1709 <lucascale1709@gmail.com>
2025-04-15 13:23:37 -0400

    claseeeprimera

--- holaaa.py
+++ holaaa.py
@@ -1,45 +1,52 @@
-# print("Ingrese sus 3 notas")
+# # print("Ingrese sus 3 notas")
 
-# n1=float(input("Ingrese su nota "))
-# n2=float(input("Ingrese su nota "))
-# n3=float(input("Ingrese su nota "))
-# promedio=float((n1+n2+n3)/3)
-# print("Su promedio es", promedio)
+# # n1=float(input("Ingrese su nota "))
+# # n2=float(input("Ingrese su nota "))
+# # n3=float(input("Ingrese su nota "))
+# # promedio=float((n1+n2+n3)/3)
+# # print("Su promedio es", promedio)
 
-# if promedio>=4:
-#     print("El alumno aprobó")
+# # if promedio>=4:
+# #     print("El alumno aprobó")
+# # else:
+# #     print("El alumno reprobó")
+
+# # print("Ingrese su edad")
+
+# # edad=int(input())
+
+# # if edad>=18:
+# #     print("Usted es mayor de edad")
+# # else:
+# #     print("Usted es menor de edad")
+
+
+
+# # print("Ingrese su edad")
+
+# # edad=int(input())
+
+# # if edad < 12:
+# #     print("Usted es un niño")
+# # elif edad >= 12 and edad <= 17:
+# #     print("Usted es adolescente")
+# # else:  
+# #     print("Usted es mayor de edad")
+
+# n1=float(input("Ingrese un número "))
+# n2=float(input("Ingrese un número "))
+# n3=float(input("Ingrese un número "))
+
+# if n1>n2 and n1>n3:
+#     print("El numero", n1 ,"es el mayor")
+# elif n2>n3:
+#     print("El número", n2 ,"es el mayor")
 # else:
-#     print("El alumno reprobó")
-
-# print("Ingrese su edad")
-
-# edad=int(input())
-
-# if edad>=18:
-#     print("Usted es mayor de edad")
-# else:
-#     print("Usted es menor de edad")
-
-
-
-# print("Ingrese su edad")
-
-# edad=int(input())
-
-# if edad < 12:
-#     print("Usted es un niño")
-# elif edad >= 12 and edad <= 17:
-#     print("Usted es adolescente")
-# else:  
-#     print("Usted es mayor de edad")
+#     print("El número", n3 ,"es el mayor")
 
 n1=float(input("Ingrese un número "))
-n2=float(input("Ingrese un número "))
-n3=float(input("Ingrese un número "))
 
-if n1>n2 and n1>n3:
-    print("El numero", n1 ,"es el mayor")
-elif n2>n3:
-    print("El número", n2 ,"es el mayor")
+if n1 % 2==0:
+    print("El número es par")
 else:
-    print("El número", n3 ,"es el mayor")
+    print("El número es impar")
